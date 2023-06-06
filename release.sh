acea61a405e09d7fff02cc6faa352835bb5f79d6
PenguinLW <diana1997525@gmail.com>
2023-06-06 19:41:01 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #in venv
-. ~/venv/bin/activate
+#. ~/venv/bin/activate
 #pip install -r req.txt
-pip freeze > req.txt
+#pip freeze > req.txt
 chmod u+x *.sh *.py
 #git clone -o onlinegdb_tmp https://github.com/PenguinLW/onlinegdb_tmp.git
 #git config --global user.email "diana1997525@gmail.com"
