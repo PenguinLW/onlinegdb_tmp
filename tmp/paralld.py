08a82fa14df0a7a2b9a5f3719ad1d6bdf9b8531d
PenguinLW <diana1997525@gmail.com>
2023-06-15 18:58:54 +0800

    PenguinL

---
+++ tmp/paralld.py
@@ -0,0 +1,35 @@
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Создание списка вершин параллелепипеда
+vertices = np.array([(0, 0, 0),
+                     (1, 0, 0),
+                     (1, 1, 0),
+                     (0, 1, 0),
+                     (0, 0, 1),
+                     (1, 0, 1),
+                     (1, 1, 1),
+                     (0, 1, 1)])
+
+# Создание списка граней параллелепипеда
+faces = np.array([(0, 1, 2, 3),
+                  (0, 1, 5, 4),
+                  (1, 2, 6, 5),
+                  (2, 3, 7, 6),
+                  (3, 0, 4, 7),
+                  (4, 5, 6, 7)])
+
+# Использование созданных списков вершин и граней для построения параллелепипеда
+ax.add_collection3d(
+    Poly3DCollection([vertices[face] for face in faces], alpha=.25, facecolor='b')
+)
+# Настройка параметроврафика
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
